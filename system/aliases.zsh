f472a10249c89c171bd780cb873971320a3aa7e8
Jeremy Frank <jeremyjfrank@gmail.com>
2017-11-21 08:43:10 -0500

    Add rm alias so that rm uses the trash can https://github.com/sindresorhus/trash

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -13,3 +13,6 @@ fi
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"
 alias ips="ifconfig -a | grep -o 'inet6\? \(addr:\)\?\s\?\(\(\([0-9]\+\.\)\{3\}[0-9]\+\)\|[a-fA-F0-9:]\+\)' | awk '{ sub(/inet6? (addr:)? ?/, \"\"); print }'"
+
+# deleting
+alias rm=trash

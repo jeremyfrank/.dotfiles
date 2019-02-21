2e93a405ea6b9767a65e5e6fe31e2e976d22fc10
Jeremy Frank <jeremyjfrank@gmail.com>
2019-02-21 15:40:39 -0500

    Add bundler to plugins list and reorder

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -10,6 +10,6 @@ ZSH_THEME="robbyrussell"
 # Which plugins would you like to load? (plugins can be found in ~/.oh-my-zsh/plugins/*)
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
-plugins=(brew gem git osx postgres ruby sublime composer)
+plugins=(brew bundler composer gem git osx postgres ruby sublime)
 
 source $ZSH/oh-my-zsh.sh

91d47407e765b71882934138709d289d64cb6aa6
Jeremy Frank <jeremyjfrank@gmail.com>
2019-11-14 15:55:10 -0500

    Fix yarn and mamp php paths

--- system/path.zsh
+++ system/path.zsh
@@ -1,3 +1,9 @@
 export PATH=/usr/local/bin:/usr/local/sbin:$PATH
-export PATH=/Applications/MAMP/Library/bin:/Applications/MAMP/bin/php/php7.0.22/bin:$PATH
-export PATH="$PATH:`yarn global bin`"
+
+# export PATH="$PATH:`yarn global bin`"
+export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
+
+# alias php to the latest PHP installed with MAMP
+MAMP_PHP__LATEST=$(ls /Applications/MAMP/bin/php/ | sort -V | tail -1)
+export PATH="/Applications/MAMP/bin/php/${MAMP_PHP__LATEST}/bin:$PATH"
+alias php="/Applications/MAMP/bin/php/${MAMP_PHP__LATEST}/bin/php -c \"/Library/Application Support/appsolute/MAMP PRO/conf/${MAMP_PHP__LATEST}.ini\""

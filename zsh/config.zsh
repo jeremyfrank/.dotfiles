96ab4662b79a52ab0dfdab42d7a21e37f8f4362b
Jeremy Frank <jeremyjfrank@gmail.com>
2019-11-16 21:45:10 -0500

    Integrate updates from holman dotfiles

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -1,9 +1,3 @@
-if [[ -n $SSH_CONNECTION ]]; then
-  export PS1='%m:%3~$(git_info_for_prompt)%# '
-else
-  export PS1='%3~$(git_info_for_prompt)%# '
-fi
-
 export LSCOLORS="exfxcxdxbxegedabagacad"
 export CLICOLOR=true
 
@@ -40,5 +34,4 @@ bindkey '^[^[[C' forward-word
 bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
-bindkey '^[^N' newtab
 bindkey '^?' backward-delete-char

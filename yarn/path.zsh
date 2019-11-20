a658bfcec13e6341c8125ed22a91a715362149c7
Jeremy Frank <jeremyjfrank@gmail.com>
2019-11-20 08:30:18 -0500

    Update yarn paths for asdf

--- yarn/path.zsh
+++ yarn/path.zsh
@@ -1,8 +1,9 @@
-# sup yarn
-# https://yarnpkg.com
+# From https://til.hashrocket.com/posts/esd7dvzfme-using-yarn-global-w-node-through-asdf-mac
 
-if (( $+commands[yarn] ))
-then
-  # export PATH="$PATH:`yarn global bin`"
-  export PATH="$HOME/.yarn/bin:$HOME/.config/yarn/global/node_modules/.bin:$PATH"
-fi
+# asdf global version manager
+source "$HOME/.asdf/asdf.sh"
+source "$HOME/.asdf/completions/asdf.bash"
+
+# set yarn binaries on path
+# must be below the .asdf source commands ^
+export PATH="$(yarn global bin):$PATH"

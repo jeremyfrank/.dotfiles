2e278fa7bfffd5c274a63a08b52c83a825e695ca
Jeremy Frank <jeremyjfrank@gmail.com>
2022-06-08 12:10:10 -0400

    Adjust asdf setup

--- zsh/oh-my-zsh.zsh
+++ zsh/oh-my-zsh.zsh
@@ -11,7 +11,6 @@ ZSH_THEME="robbyrussell"
 # Custom plugins may be added to ~/.oh-my-zsh/custom/plugins/
 # Example format: plugins=(rails git textmate ruby lighthouse)
 plugins=(
-  asdf
   brew
   bundler
   composer

a1cc0a9d9b712b1f98a44d28f15d9c17527b9fac
Jeremy Frank <jeremyjfrank@gmail.com>
2022-06-08 12:10:45 -0400

    Remove duplicate system aliases

--- system/aliases.zsh
+++ system/aliases.zsh
@@ -1,14 +1,3 @@
-# grc overides for ls
-#   Made possible through contributions from generous benefactors like
-#   `brew install coreutils`
-if $(gls &>/dev/null)
-then
-  alias ls="gls -F --color"
-  alias l="gls -lAh --color"
-  alias ll="gls -l --color"
-  alias la='gls -A --color'
-fi
-
 # IP addresses
 alias ip="dig +short myip.opendns.com @resolver1.opendns.com"
 alias localip="ipconfig getifaddr en1"

f3608bec336115425f245169a6468bec76c2a318
Jeremy Frank <jeremyjfrank@gmail.com>
2026-01-06 21:41:51 -0500

    Remove unused yarn config

--- yarn/path.zsh
+++
@@ -1,9 +0,0 @@
-# From https://til.hashrocket.com/posts/esd7dvzfme-using-yarn-global-w-node-through-asdf-mac
-
-# asdf global version manager
-# source "$HOME/.asdf/asdf.sh"
-# source "$HOME/.asdf/completions/asdf.bash"
-
-# set yarn binaries on path
-# must be below the .asdf source commands ^
-# export PATH="$(yarn global bin):$PATH"
